55d5c933b4cf2537814672b6ba11feb52c74d8dc
Pedro <pedro040529@gmail.com>
2019-06-12 07:16:21 -0500

    segundo

--- Donaciones.php
+++ Donaciones.php
@@ -1,44 +1,4 @@
-
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="diseño.css">
-
-    <title>Proyecto</title>
-</head>
-<body>
-    
-    <header>
-        <div class="ancho">
-            <div class="logo">
-                  <p><a href="index.php">Albergue</a></p>
-            </div>
-            <nav>
-                <ul>
-                     <li><a href="index.php">Inicio</a></li>
-                     <li><a href="Nosotros.php">Nosotros</a></li>
-                     <li><a href="Adopciones.php">Adopciones</a></li>
-                     <li><a href="Voluntariado.php">Voluntariado</a></li>
-                     <li><a href="Donaciones.php">Donaciones</a></li>
-                     <li><a href="#">Contacto</a></li>
-                </ul>                  
-            </nav>
-        </div>
-    </header>
-    <div class="fondo">
-
-        <h1 class="tituloletra">Ellos también te necesitan </h1>
-            <pre>
-                <p class="contenido">Los perros de El Poder de la Manada tienen una característica 
-especial que nos inspira día a día, es que ellos, a pesar de
-sus tristes historias, siempre nos esperan con una sonrisa.</p>
-            </pre>
-            </div>
-            <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -137,8 +97,5 @@ aplicando la técnica de socialización activa entre pares</p>
             </div>
         
     </footer>
-</body>
-</html>
-  
 </body>
 </html>
---
+++ Nosotros.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="diseño.css">
+
+    <title>Proyecto</title>
+</head>
+<body>
+    <header>
+        <div class="ancho">
+            <div class="logo">
+                  <p><a href="Nosotros.php">Albergue</a></p>
+            </div>
+            <nav>
+                <ul>
+                     <li><a href="index.php" class="abc">Inicio</a></li>
+                     <li><a href="Nosotros.php" class="abc">Nosotros</a></li>
+                     <li><a href="Adopciones.php" class="abc">Adopciones</a></li>
+                     <li><a href="Voluntariado.php" class="abc">Voluntariado</a></li>
+                     <li><a href="Donaciones.php" class="abc">Donaciones</a></li>
+                     <li><a href="#">Contacto</a></li>
+                </ul>                  
+            </nav>
+        </div>
+    </header>
+    <div class="fondo2">
+    <div class="colordiv">
+        <h1 class="titulo2">NUESTRO ALBERGUE</h1>
+            <p class="contenido2">No somos solo amantes de los animales. Somos un grupo especializado y con años de experiencia, reconocidos a nivel nacional.</p>
+        
+    </div>
+    
+           
+                <p class="contenido2">El Poder de la Manada es un centro de rehabilitación canino, sin fines de lucro, que está enfocado principalmente en perros que sufren 
+                alguna enfermedad grave o tienen problemas conductuales. Somos un equipo de profesionales que cree fervientemente que podemos darles una 
+                segunda oportunidad a estos canes, aplicando la técnica de socialización activa entre pares. Nuestro modo de trabajo se rige por las 
+                4 R’s, lo que significa: Rescate, Rehabilitación, Reeducación y Reinserción</p>
+
+                <h1 class="titulo2">Misión</h1>
+
+                <p class="contenido2">Somos una fundación sin fines de lucro que buscamos educar a la población en cuanto a la tenencia responsable de mascota para disminuir la cantidad de canes callejeros y/o abandonados. 
+                    También rescatamos y rehabilitamos a perros en situación de calle y abandono y los reinsertamos en la comunidad.</p>
+
+                <h1 class="titulo2">Visión</h1>
+
+                <p class="contenido2">Constituirse en un referente en el cuidado y rehabilitación de canes en situación desmejorada ( abandonados por conductas o por situación física) y en un ente habilitado para encausar programas,
+                         ayudas o subsidios relacionados con los propósitos de reinserción y concientización en la tenencia responsable.</p>
+
+                <h1 class="titulo2">Nuestros objetivos</h1>
+                <p class="contenido2">1. El rescate de animales maltratados, abandonados y en situación, física y psicológica, desmejorada para luego trabajar en su rehabilitación, protección y adiestramiento.</p>
+                <p class="contenido2">2. Organizar y/o patrocinar campañas de educación, fomento y divulgación de la tenencia responsable.</p>
+                <p class="contenido2">3. Realizar operativos de vacunación, esterilización, desparasitación y adiestramiento de los animales que se encuentran abandonados en las calles.</p>
+                <p class="contenido2">4. Mantenimiento de uno o más refugios para la adecuada protección y cuidado de los animales en situación desmejorada.</p>
+                <p class="contenido2">5. Realizar todo tipo de campañas de difusión, estudios, jornadas de capacitación, entre otros, sobre las leyes existentes en materia de protección animal.</p>
+                <p class="contenido2">6. Suscribir convenios con otras fundaciones, con fines u objetivos similares y convenios de asistencia técnica, veterinaria, educacional y de acción social con entes públicos o privados.</p>
+                <p class="contenido2">7. Capacitar y especializar en materias de adiestramiento y manejo conductual de los animales, medicina veterinaria y administración.</p>
+                <p class="contenido2">8. Promover y difundir nuestro modelo de rehabilitación (socialización activa entre pares).</p>
+                <p class="contenido2">9. Construir y administrar una clínica veterinaria para prestar servicios a la comunidad a bajo costo, y que en ella puedan tratarse los animales abandonados y refugiados.</p>
+               
+</div>
+                <footer>
+       
+       <div class="container-footer-all">
+        
+            <div class="container-body">
+
+                <div class="colum1">
+                    <h1>Mas informacion de Nosotros</h1>
+                    <pre>
+                    <p>Somos un equipo de profesionales que cree fervientemente 
+que podemos darles una segunda oportunidad a estos canes, 
+aplicando la técnica de socialización activa entre pares</p>
+                    </pre>
+
+                </div>
+
+                <div class="colum2">
+
+                    <h1>Redes Sociales</h1>
+
+                    <div class="row">
+                        <img src="iconos/facebook.png">
+                        <label>Siguenos en Facebook</label>
+                    </div>
+                    <div class="row">
+                        <img src="iconos/twitter.png">
+                        <label>Siguenos en Twitter</label>
+                    </div>
+                    <div class="row">
+                        <img src="iconos/instagram.png">
+                        <label>Siguenos en Instagram</label>
+                    </div>
+
+                </div>
+
+                <div class="colum3">
+
+                    <h1>Informacion Contactos</h1>
+
+                    <div class="row2">
+                        <img src="iconos/casa.png">
+                        <label>Av. la Fontana 550, La Molina 00012</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="iconos/celular.png">
+                        <label>+51 987654321</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="iconos/contacto.png">
+                         <label>pedro040529@gmail.com</label>
+                    </div>
+
+                </div>
+
+            </div>
+        
+        </div>
+        
+        <div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2019 Todos los Derechos Reservados 
+                    </div>
+                </div>
+
+            </div>
+        
+    </footer>
+</body>
+</html>
